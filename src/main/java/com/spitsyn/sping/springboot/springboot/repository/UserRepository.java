8dee3837b3e6a0e46bbeafa1e31520066f27fb5a
Ilya Spitsyn <mynamme1@gmail.com>
2021-04-29 14:11:29 +0300

    Create LAZY download with JOIN FETCH

--- src/main/java/com/spitsyn/sping/springboot/springboot/repository/UserRepository.java
+++ src/main/java/com/spitsyn/sping/springboot/springboot/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.spitsyn.sping.springboot.springboot.repository;
 
 import com.spitsyn.sping.springboot.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("from User u JOIN FETCH u.roles as roles WHERE u.username = :username")
     User findByUsername(String username);
 
 }
